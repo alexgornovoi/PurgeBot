4f7242246a4ccd5a7e11706127c115bf65a4c8f2
alexgornovoi <alex.gornovoi@gmail.com>
2023-09-13 12:00:19 -0400

    added clear messages for users and bot

--- incogbot.py
+++ incogbot.py
@@ -1,5 +1,6 @@
 import discord
 import os
+import datetime
 from dotenv import load_dotenv
 from discord import app_commands
 
@@ -28,12 +29,28 @@ async def first_command(interaction):
 
 @client.event
 async def on_ready():
-    channel = client.get_channel(824163732518731779)
-    print(channel)
     await tree.sync(guild=discord.Object(id=guildId))
     print("Ready!")
-    async for message in channel.history():
-        print(message.created_at)
+
+@tree.command(name = "clear", description = "Clears all messages sent after a certain amount of days", guild=discord.Object(id=guildId)) 
+@app_commands.describe(days="Delete all messages from before x days ago")
+@app_commands.rename(days='days')
+async def clear(interaction, days:int):
+    def is_me(m):
+        return m.author == interaction.user
+    channel = client.get_channel(interaction.channel_id)
+    deleted = await channel.purge(limit=100, check=is_me, before=datetime.datetime.utcnow()-datetime.timedelta(days))
+    await channel.send(f'Deleted {len(deleted)} message(s) from user {interaction.user}')
+
+@tree.command(name = "clear_bot", description = "Clears all bot messages", guild=discord.Object(id=guildId)) 
+async def clear_bot(interaction):
+    def is_incog(m):
+        return m.author == client.user
+    channel = client.get_channel(interaction.channel_id)
+    deleted = await channel.purge(limit=1000, check=is_incog)
+    await channel.send(f'Deleted {len(deleted)} message(s) from user {client.user}')
+
+    
 
 
 client.run(os.getenv('token'))
