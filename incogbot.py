8415d1b6c2482c93ef34c3844ffc0c6031b23b37
alexgornovoi <alex.gornovoi@gmail.com>
2023-09-21 10:56:48 -0400

    created utils

--- incogbot.py
+++ incogbot.py
@@ -4,6 +4,7 @@ import datetime
 import time
 from dotenv import load_dotenv
 from discord import app_commands
+import utils
 
 load_dotenv()
 
@@ -28,36 +29,15 @@ async def first_command(interaction):
 @app_commands.describe(days="Delete all messages from before x days ago")
 @app_commands.rename(days='days')
 async def clear(interaction, days:int):
-    await interaction.response.send_message(f'Clearing {interaction.user}\'s messages. This may take a while...')
-    
-    def is_me(m):
-        return m.author == interaction.user
-    
-    channel = client.get_channel(interaction.channel_id)
-    last = datetime.datetime.utcnow()-datetime.timedelta(days)
-    total = 0
-    finished = False
-    
-    while not finished:
-        messages = [message async for message in channel.history(limit=100,before=last)]
-        if len(messages)!=100:
-            finished = True
-        deleted = await channel.purge(limit=100, check=is_me, before=last)
-        total += len(deleted)
-        time.sleep(5)
-        last = messages[-1].created_at
-        print(f'Deleted {total} messages')
-        
-    await channel.send(f'Deleted {total} message(s) from user {interaction.user}')
+    await utils.clear(days, client, interaction, interaction.user)
 
 @tree.command(name = "clear_bot", description = "Clears all IncogBots messages.", guild=discord.Object(id=guildId)) 
 async def clear_bot(interaction):
-    await interaction.response.send_message("Clearing bot messages...")
-    def is_incog(m):
-        return m.author == client.user
-    channel = client.get_channel(interaction.channel_id)
-    deleted = await channel.purge(limit=100, check=is_incog)
-    await channel.send(f'Deleted {len(deleted)} message(s) from user {client.user}')
+    await utils.clear(0, client, interaction, client.user)
+    
+@tree.command(name = "clear_all", description = "Clears all messages from all users.", guild=discord.Object(id=guildId)) 
+async def clear_bot(interaction):
+    await utils.clear_all(client, interaction)
 
     
 
---
+++ utils.py
@@ -0,0 +1,47 @@
+import datetime
+import time
+
+def is_user(user):
+  def check(m):
+    return m.author == user
+  return check
+
+async def clear(days, client, interaction, user):
+    channel = client.get_channel(interaction.channel_id)
+    last = datetime.datetime.utcnow()-datetime.timedelta(days)
+    total = 0
+    finished = False
+    
+    await interaction.response.send_message(f'Clearing {interaction.user}\'s messages. This may take a while...')
+    
+    while not finished:
+        messages = [message async for message in channel.history(limit=100,before=last)]
+        if len(messages)!=100:
+            finished = True
+        deleted = await channel.purge(limit=100, check=is_user(user), before=last)
+        total += len(deleted)
+        time.sleep(5)
+        last = messages[-1].created_at
+        print(f'Deleted {total} messages')
+    
+    await channel.send(f'Deleted {total} message(s) from user {interaction.user}')
+    
+async def clear_all(client, interaction):
+    channel = client.get_channel(interaction.channel_id)
+    last = datetime.datetime.utcnow()
+    total = 0
+    finished = False
+    
+    await interaction.response.send_message(f'Clearing all messages. This may take a while...')
+    
+    while not finished:
+        messages = [message async for message in channel.history(limit=100,before=last)]
+        if len(messages)!=100:
+            finished = True
+        deleted = await channel.purge(limit=100,  before=last)
+        total += len(deleted)
+        time.sleep(5)
+        last = messages[-1].created_at
+        print(f'Deleted {total} messages')
+    
+    await channel.send(f'Deleted {total} message(s) from user channel.')
